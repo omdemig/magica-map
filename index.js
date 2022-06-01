e15eed1fa493a5976bf39426453db48a7982dc8c
omdemig <106059777+omdemig@users.noreply.github.com>
2022-06-01 18:51:26 +1000

    Ammending touch support featuresto make divs play nice

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(e) {
     // make image map coords adapt to a resized map
-    $('img[usemap]').rwdImageMaps(); 
+    $("img[usemap]").rwdImageMaps(); 
     // highlight our choices!
-    $('img[usemap]').maphilight();
+    $("img[usemap]").maphilight();
     
     // don't highlight three starting regions in a group if they are selected individually
     $("#start1, #start2, #start3").mouseover(function(e){
@@ -12,11 +12,19 @@ $(document).ready(function(e) {
         $("#start1, #start2, #start3").data("maphilight",starterData);
     }); 
 
+     // handling for when something other than a building is clicked
+     $(":not(area)").on("click", function(e){
+        e.preventDefault(); 
+        $(".banner").text("");
+        e.stopPropagation();
+    });
+
     // for friends with no hover
-    $('area').on("click", function(e){
+    $("area").on("click", function(e){
         e.preventDefault(); 
         // show the image map area's title in a banner box
-        $(".banner").text(e.currentTarget.title);
+        $(".banner").text("✸   "+e.currentTarget.title);
+        e.stopPropagation();
     });
 
 });
